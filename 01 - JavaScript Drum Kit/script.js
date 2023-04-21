18f3ec718af5b9958d92bca42c741b008c1f539b
NowWeAre-Free <kontodarm@wp.pl>
2023-04-21 21:35:19 +0200

    add line which change border to clicked button

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,4 +1,8 @@
 window.addEventListener("keydown", function (e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+  if (!audio) return;
   audio.play();
+  key.classList.add("playing");
 });
