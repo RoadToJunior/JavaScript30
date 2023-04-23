c3b87eb7831f704420407c69c74008076314f88a
NowWeAre-Free <kontodarm@wp.pl>
2023-04-23 20:12:34 +0200

    add remove transition function

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,4 +1,9 @@
-window.addEventListener("keydown", function (e) {
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  e.target.classList.remove("playing");
+}
+
+function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
@@ -6,4 +11,8 @@ window.addEventListener("keydown", function (e) {
   audio.currentTime = 0;
   audio.play();
   key.classList.add("playing");
-});
+}
+
+const keys = Array.from(document.querySelectorAll(".key"));
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+window.addEventListener("keydown", playSound);
