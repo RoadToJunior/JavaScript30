098ecf54c8333d4a7bc243f382129facd614dddd
NowWeAre-Free <kontodarm@wp.pl>
2023-04-24 20:01:13 +0200

    add transform-origin to CSS

--- 02 - JS and CSS Clock/script.js
+++ 02 - JS and CSS Clock/script.js
@@ -1,5 +1,12 @@
+const second = document.querySelector(".second-hand");
+const min = document.querySelector(".min-hand");
+
 function setDate() {
   const now = new Date();
-  const seconds = now.getSeconds;
+  const seconds = now.getSeconds();
+  const min = now.ge;
   const secondsToDegress = (seconds / 60) * 360 + 90;
+  second.style.transform = `rotate(${secondsToDegress}deg)`;
 }
+
+setInterval(setDate, 1000);
