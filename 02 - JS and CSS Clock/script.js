6ca23014bc18a0aa6b8096356e34cc350eb7e99c
NowWeAre-Free <kontodarm@wp.pl>
2023-04-24 20:02:27 +0200

    add consts

--- 02 - JS and CSS Clock/script.js
+++ 02 - JS and CSS Clock/script.js
@@ -1,10 +1,12 @@
 const second = document.querySelector(".second-hand");
 const min = document.querySelector(".min-hand");
+const hour = document.querySelector(".second-hand");
 
 function setDate() {
   const now = new Date();
   const seconds = now.getSeconds();
-  const min = now.ge;
+  const min = now.getMinutes();
+  const hour = now.getHours();
   const secondsToDegress = (seconds / 60) * 360 + 90;
   second.style.transform = `rotate(${secondsToDegress}deg)`;
 }
