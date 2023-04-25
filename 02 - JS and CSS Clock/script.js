eaffc05491013faa7e0d209288afe69655a3e74f
NowWeAre-Free <kontodarm@wp.pl>
2023-04-25 20:09:23 +0200

    add minute and hour style transform

--- 02 - JS and CSS Clock/script.js
+++ 02 - JS and CSS Clock/script.js
@@ -1,14 +1,21 @@
 const second = document.querySelector(".second-hand");
-const min = document.querySelector(".min-hand");
+const minute = document.querySelector(".min-hand");
 const hour = document.querySelector(".second-hand");
 
 function setDate() {
   const now = new Date();
+
   const seconds = now.getSeconds();
-  const min = now.getMinutes();
-  const hour = now.getHours();
   const secondsToDegress = (seconds / 60) * 360 + 90;
   second.style.transform = `rotate(${secondsToDegress}deg)`;
+
+  const minutes = now.getMinutes();
+  const minutesToDegress = (minutes / 60) * 360 + 90;
+  minute.style.transform = `rotate(${minutesToDegress}deg)`;
+
+  const hours = now.getMinutes();
+  const hoursToDegress = (hours / 12) * 360 + 90;
+  hour.style.transform = `rotate(${hoursToDegress}deg)`;
 }
 
 setInterval(setDate, 1000);
